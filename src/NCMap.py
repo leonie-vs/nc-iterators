59e9135b17661e01507b2b1f680076d0a06e890b
shohag1610 <shohag.edu96@gmail.com>
2025-10-07 14:58:45 +0100

    Map: second step done

--- src/NCMap.py
+++ src/NCMap.py
@@ -1,3 +1,34 @@
 class NCMap:
-    def __init__(self):
-        pass
+    def __init__(self, func, list):
+        self.func = func
+        self.list = list
+        self.index = 0
+
+        print(self.func)
+        
+    def __iter__(self):
+        return self 
+    
+    def __next__(self):
+        try:
+            result = self.func(self.list[self.index])
+            self.index +=1
+            return result
+        except IndexError:
+            raise StopIteration
+
+
+
+
+
+
+
+
+
+
+
+
+def demo():
+    pass
+
+e = NCMap(demo, ['a', 'b', 'c', 'd'])
--- test/test_NCMap.py
+++ test/test_NCMap.py
@@ -1,2 +1,54 @@
 from src.NCMap import NCMap
+import pytest
+
+def test_iter_returns_reference_to_enumerate_instance():
+
+    def demo_mul_5(item):
+        return item * 5
+    
+    list = [3]
+    
+    nm = NCMap(demo_mul_5, list)
+
+    assert nm is iter(nm)
+
+
+def test_next_should_call_funct_with_single_item():
+
+    def demo_mul_5(item):
+        return item * 5
+    
+    list = [3]
+    
+    nm = NCMap(demo_mul_5, list)
+
+    assert next(nm) == 15
+
+
+def test_next_should_call_funct_with_multiple_item():
+
+    def demo_mul_5(item):
+        return item * 5
+    
+    list = [3,4]
+    
+    nm = NCMap(demo_mul_5, list)
+
+    assert next(nm) == 15
+    assert next(nm) == 20
+
+def test_next_should_raise_stop_iteration():
+    def demo_mul_5(item):
+        return item * 5
+    
+    list = [3,4]
+    
+    nm = NCMap(demo_mul_5, list)
+
+    assert next(nm) == 15
+    assert next(nm) == 20
+
+    with pytest.raises(StopIteration):
+        next(nm)
+
 
