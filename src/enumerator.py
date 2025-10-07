7959585fc8cd792781cfdda7cbdef8d7bb6ccf58
shohag1610 <shohag.edu96@gmail.com>
2025-10-07 11:48:08 +0100

    First: done with test cases

--- src/enumerator.py
+++ src/enumerator.py
@@ -1,12 +1,28 @@
 class NCEnumerate:
     def __init__(self, lst):
-        # implement me
-        pass
+        
+        self.iterable = lst
 
     def __iter__(self):
-        # implement me
-        pass
+        return self
 
     def __next__(self):
         # implement me
         pass
+
+
+
+
+
+
+# def enumerates(iterable, start=0):
+#     n = start
+#     for elem in iterable:
+#         yield n, elem
+#         n += 1
+
+# list = ['a','b','c']
+
+# item = enumerates(list, start=1)
+# print(next(item))
+# print(next(item))
--- test/test_enumerator.py
+++ test/test_enumerator.py
@@ -2,5 +2,6 @@ from src.enumerator import NCEnumerate
 
 
 def test_iter_returns_reference_to_enumerate_instance():
-    # implement me
-    pass
+    e = NCEnumerate(['a', 'b', 'c', 'd'])
+
+    assert e is iter(e)
